33789daea7eb027dab5f2052073039598d7bdaea
vitor branquinho <va42622@gmail.com>
2025-07-30 23:18:36 -0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install system dependencies required for PyArrow
-apt-get update && apt-get install -y \
-    cmake \
-    libboost-all-dev \
-    libssl-dev
-
-# Install Python packages with wheel preference
-pip install --prefer-binary -r requirements.txt
