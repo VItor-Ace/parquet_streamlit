06ac570e1fcfcea7e5982cd4a980b82d442ead7a
vitor branquinho <va42622@gmail.com>
2025-07-30 22:54:57 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ apt-get update && apt-get install -y \
     cmake \
     libboost-all-dev \
     libssl-dev
+
+# Install Python packages with wheel preference
+pip install --prefer-binary -r requirements.txt
